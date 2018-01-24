e11cd75b8f9d06dd6fe81416763842d2f112d760
Hein de Wilde <hp.dewilde@gmail.com>
2018-01-25 00:38:23 +0100

    Update db.py

--- db.py
+++ db.py
@@ -9,20 +9,13 @@ r = requests.get(url)
 
 output = r.json()
 
-for i in range(10):
-    for i in range(len(output['results'])):
-        result = output['results'][i]
-        question = result['question']
-        correct_answer = result['correct_answer']
-        wrong_answer1 = result['incorrect_answers'][0]
-        wrong_answer2 = result['incorrect_answers'][1]
-        wrong_answer3 = result['incorrect_answers'][2]
+for i in range(len(output['results'])):
+    result = output['results'][i]
+    question = result['question']
+    correct_answer = result['correct_answer']
+    wrong_answer1 = result['incorrect_answers'][0]
+    wrong_answer2 = result['incorrect_answers'][1]
+    wrong_answer3 = result['incorrect_answers'][2]
 
-        db.execute("INSERT INTO questions (question, correct_answer, wrong_answer1, wrong_answer2, wrong_answer3) VALUES (:question, :correct_answer, :wrong_answer1, :wrong_answer2, :wrong_answer3)", question=question, correct_answer=correct_answer, wrong_answer1=wrong_answer1, wrong_answer2=wrong_answer2, wrong_answer3=wrong_answer3)
+    db.execute("INSERT INTO questions (question, correct_answer, wrong_answer1, wrong_answer2, wrong_answer3) VALUES (:question, :correct_answer, :wrong_answer1, :wrong_answer2, :wrong_answer3)", question=question, correct_answer=correct_answer, wrong_answer1=wrong_answer1, wrong_answer2=wrong_answer2, wrong_answer3=wrong_answer3)
 
-    """
-    print("Question: {}".format(question))
-    print("Correct answer: {}".format(correct_answer))
-    print("Incorrect answers: {}".format(incorrect_answers))
-    print()
-    """
